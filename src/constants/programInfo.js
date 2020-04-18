4d05237911a295505559b1a7b72304afd709d0a1
Chris DeSilva <chris@skills.fund>
2020-04-18 16:29:17 -0500

    New template

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -1,245 +1,256 @@
-import logo from '../images/logo_ia.jpg'; // add school logo to images folder and import here
+import logo from "../images/logo_ia.jpg" // add school logo to images folder and import here
 
-export const applicationsLive = true; // set to false if not currently accepting applications
-export const disabledLoanAppFormID = '69140c47-bb03-4a1b-b674-cc136fdea23d'; // Hubspot ID for all disabled apply now lead captures
-export const nextCohortStartDate = 'October 29, 2019';
+export const applicationsLive = true // set to false if not currently accepting applications
+export const disabledLoanAppFormID = "69140c47-bb03-4a1b-b674-cc136fdea23d" // Hubspot ID for all disabled apply now lead captures
+export const nextCohortStartDate = "October 29, 2019"
 
 // ***** BEGIN TERMS AND FAQ INFO *****
 
 export const schoolInfo = {
-	// term details section
-	interestRate36: '9.49%',
-	interestRate60: '11.46%',
-	APRRange36: '12.20%',
-	APRRange60: '13.19%',
-	interestOnly: [
-		{
-			// for multiple examples of interest only, add items to interestOnly array
-			programName: 'PROGRAM NAME',
-			APR36: 'XX.XX%',
-			financeCharge36: '$X,XXX.XX',
-			IOPayment36: '$XX.XX',
-			FullMonthlyPayment36: '$XXX.XX',
-			APR60: 'XX.XX%',
-			financeCharge60: '$X,XXX.XX',
-			IOPayment60: '$XX.XX',
-			FullMonthlyPayment60: '$XXX.XX',
-			LoanExampleAmt: '$10,000',
-			LoanExampleOFeeAmt: '$400',
-			LoanExampleAmtPlusOFee: '$10,400',
-			programLength: 'X' // program length in months
-		}
-	],
-	immediateRepayment: [
-		{
-			// for multiple examples of immmediate repayment, add items to immediateRepayment array
-			programName: '',
-			APR36: '12.20%',
-			financeCharge36: '$1,991.39',
-			FullMonthlyPayment36: '$333.09',
-			APR60: '13.19%',
-			financeCharge60: '$3,710.86',
-			FullMonthlyPayment60: '$228.51',
-			LoanExampleAmt: '$10,000',
-			LoanExampleOFeeAmt: '$400',
-			LoanExampleAmtPlusOFee: '$10,400'
-		}
-	]
-};
+  // term details section
+  interestRate36: "9.49%",
+  interestRate60: "11.46%",
+  APRRange36: "12.87%",
+  APRRange60: "13.62%",
+  interestOnly: [
+    {
+      // for multiple examples of interest only, add items to interestOnly array
+      programName: "PROGRAM NAME",
+      APR36: "XX.XX%",
+      financeCharge36: "$X,XXX.XX",
+      IOPayment36: "$XX.XX",
+      FullMonthlyPayment36: "$XXX.XX",
+      APR60: "XX.XX%",
+      financeCharge60: "$X,XXX.XX",
+      IOPayment60: "$XX.XX",
+      FullMonthlyPayment60: "$XXX.XX",
+      LoanExampleAmt: "$10,000",
+      LoanExampleOFeeAmt: "$400",
+      LoanExampleAmtPlusOFee: "$10,400",
+      programLength: "X", // program length in months
+    },
+  ],
+  immediateRepayment: [
+    {
+      // for multiple examples of immmediate repayment, add items to immediateRepayment array
+      programName: "",
+      APR36: "12.87%",
+      financeCharge36: "$1,991.39",
+      FullMonthlyPayment36: "$333.09",
+      APR60: "13.62%",
+      financeCharge60: "$3,710.86",
+      FullMonthlyPayment60: "$228.51",
+      LoanExampleAmt: "$10,000",
+      LoanExampleOFeeAmt: "$400",
+      LoanExampleAmtPlusOFee: "$10,400",
+    },
+  ],
+}
 
 export const faq = {
-	// faq section
-	costOfLiving: false, // true if at least one program has cost of living included
-	costOfLivingPrograms: '', // leave as empty string is cost of living availability is the same across all programs
-	multCostOfLivingPrograms: false, // true if costOfLivingPrograms string has more than one program
-	interestOnly: false, // true if interest-only payments are an option
-	immediateRepayment: true, // true if immediate repayment is an option
-	multipleLoanLengths: true, // true if 36 and 60 month options are both available
-	multipleLoanTypes: false, // true if both IR and IO are available
-	multiPrograms: true, // only true if there are multiple programs
-	onlinePrograms: true, // true if at least one program is remote/online
-	schoolHQState: 'TX',
-	origFee: 0.04,
+  // faq section
+  costOfLiving: false, // true if at least one program has cost of living included
+  costOfLivingPrograms: "", // leave as empty string is cost of living availability is the same across all programs
+  multCostOfLivingPrograms: false, // true if costOfLivingPrograms string has more than one program
+  interestOnly: false, // true if interest-only payments are an option
+  immediateRepayment: true, // true if immediate repayment is an option
+  multipleLoanLengths: true, // true if 36 and 60 month options are both available
+  multipleLoanTypes: false, // true if both IR and IO are available
+  multiPrograms: true, // only true if there are multiple programs
+  onlinePrograms: true, // true if at least one program is remote/online
+  schoolHQState: "TX",
+  origFee: 0.05,
 
-	// interest payment FAQ info
-	exampleLoanAmount: '$10,000',
-	interestRate36: '8.99%',
-	interestRate60: '10.99%',
-	APR36: 'XX.XX - XX.XX%',
-	APR60: 'XX.XX - XX.XX%',
-	IOPayment36: '$77.91',
-	IOPayment60: '$95.25',
+  // interest payment FAQ info
+  exampleLoanAmount: "$10,000",
+  interestRate36: "8.99%",
+  interestRate60: "10.99%",
+  APR36: "XX.XX - XX.XX%",
+  APR60: "XX.XX - XX.XX%",
+  IOPayment36: "$77.91",
+  IOPayment60: "$95.25",
 
-	// max loan amounts by program for faq1
-	loanRange: [
-		{
-			programName: "Inventive Academy's Full Stack Immersive In-Person program",
-			maxAmount: '$13,000',
-			col: false,
-			colAmount: '$6,000'
-		},
-		{
-			programName: "Inventive Academy's Full Stack Immersive Online program",
-			maxAmount: '$11,000',
-			col: false,
-			colAmount: '$6,000'
-		}
-	]
-};
+  // max loan amounts by program for faq1
+  loanRange: [
+    {
+      programName: "Inventive Academy's Full Stack Immersive In-Person program",
+      maxAmount: "$13,000",
+      col: false,
+      colAmount: "$6,000",
+    },
+    {
+      programName: "Inventive Academy's Full Stack Immersive Online program",
+      maxAmount: "$11,000",
+      col: false,
+      colAmount: "$6,000",
+    },
+  ],
+}
 
 // ***** END TERMS AND FAQ INFO *****
 
 // ***** BEGIN GENERAL SCHOOL INFO *****
 
-export const schoolLogo = logo; // go to header.js if height needs adjustment
+export const schoolLogo = logo // go to header.js if height needs adjustment
 
-export const schoolName = 'Inventive Academy';
+export const schoolName = "Inventive Academy"
 
-export const schoolURL = 'https://inventive.io/'; // update with url of school's website
+export const schoolURL = "https://inventive.io/" // update with url of school's website
 
-export const skfURL = 'https://inventive.skills.fund/'; // update with Skills Fund url
+export const skfURL = "https://inventive.skills.fund/" // update with Skills Fund url
 
-export const headline = 'Learn to Code at Inventive Academy'; // update headline as appropriate
+export const headline = "Learn to Code at Inventive Academy" // update headline as appropriate
 
 export const leadContent = {
-	header: 'Your last step on the path toward changing your career',
-	paragraph: `${schoolName} designs its programs to provide students a foundation for a career in software development. ${schoolName} partners with Skills Fund to offer tuition${faq.costOfLiving
-		? ' and cost of living'
-		: ''} financing so more students like you can access their program.`
-};
+  header: "Your last step on the path toward changing your career",
+  paragraph: `${schoolName} designs its programs to provide students a foundation for a career in software development. ${schoolName} partners with Skills Fund to offer tuition${
+    faq.costOfLiving ? " and cost of living" : ""
+  } financing so more students like you can access their program.`,
+}
 
 export const threeStepCardText = {
-	step1: '',
-	step2: {
-		header: 'select your program',
-		text: 'Choose between the Full-Stack Immersive In-Person or Full-Stack Immersive Online programs.'
-	},
-	step3: `You'll be on your way to an exciting career in tech as part of ${schoolName}'s powerful network.`
-};
+  step1: "",
+  step2: {
+    header: "select your program",
+    text:
+      "Choose between the Full-Stack Immersive In-Person or Full-Stack Immersive Online programs.",
+  },
+  step3: `You'll be on your way to an exciting career in tech as part of ${schoolName}'s powerful network.`,
+}
 
-export const netlifyFormName = 'inventiveacademy_contact';
+export const netlifyFormName = "inventiveacademy_contact"
 
-export const GATracking = 'UA-68312423-1';
+export const GATracking = "UA-68312423-1"
 
-export const hubspotFormId = '660631b9-aee4-4d9a-8bb9-2c8b1a1a2143'; // create Hubspot form, get form id after publishing
+export const hubspotFormId = "660631b9-aee4-4d9a-8bb9-2c8b1a1a2143" // create Hubspot form, get form id after publishing
 
-export const selectAProgram = 'select_a_inventiveacademy_program'; // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
+export const selectAProgram = "select_a_inventiveacademy_program" // update school name to match form field on Hubspot, *** change to "program_name" if only one program ***"
 
 // ***** END GENERAL SCHOOL INFO *****
 
 // ***** BEGIN LOAN APP AND CALC INFO *****
 
-export const defaultLoanAmount = 10000;
-export const placeholder = '$10,000';
+export const defaultLoanAmount = 10000
+export const placeholder = "$10,000"
 export const interestRates = {
-	ir36: 9.49,
-	ir60: 11.46
-};
-export const moreThanSixPrograms = false; // set to true if there are 7 or more programs in the loan application. True will render a dropdown menu, false will render buttons for each program.
+  ir36: 9.49,
+  ir60: 11.46,
+}
+export const moreThanSixPrograms = false // set to true if there are 7 or more programs in the loan application. True will render a dropdown menu, false will render buttons for each program.
 
 export const programLoanInfo = [
-	// update with program names and corresponding loan URLs with market segment code from Master Loan Parameters
-	{
-		name: 'Full-Stack Immersive In-Person',
-		url: 'https://my.skills.fund/application?lenderCode=SKINVTI18',
-		loanInfo: {
-			// match loanInfo in first metro below
-			maxLoanAmt: 13000,
-			loanTerm36: true,
-			loanTerm60: true,
-			'0': null,
-			'1': {
-				apr36: 12.2,
-				apr60: 13.19
-			}
-		},
-		defaultLoanType: '1', // leave at 0 for interest-only, set to 1 for immediate repayment
-		showMetros: false, // true if there are multiple metros with different tuition amounts for the same program
-		showLoanTypes: false, // true if both IR and IO are available
-		locations: [ 'Metro 1', 'Metro 2', 'Metro 3' ],
-		metros: [
-			// list in same order as locations array above
-			{
-				location: 'Metro 1',
-				loanInfo: {
-					// // match loanInfo to Program 1 above
-					maxLoanAmt: 13000,
-					loanTerm36: true,
-					loanTerm60: true,
-					'0': null,
-					'1': {
-						apr36: 12.2,
-						apr60: 13.19
-					}
-				}
-			}
-		]
-	},
-	{
-		name: 'Full-Stack Immersive Online',
-		url: 'https://my.skills.fund/application?lenderCode=SKINVTO18',
-		loanInfo: {
-			// match loanInfo in first metro below
-			maxLoanAmt: 11000,
-			loanTerm36: true,
-			loanTerm60: true,
-			'0': null,
-			'1': {
-				apr36: 12.2,
-				apr60: 13.19
-			}
-		},
-		defaultLoanType: '1', // leave at 0 for interest-only, set to 1 for immediate repayment
-		showMetros: false,
-		showLoanTypes: false,
-		locations: [ 'Metro 1', 'Metro 2' ],
-		metros: [
-			{
-				location: 'Metro 1',
-				loanInfo: {
-					// match loanInfo to Program 2 loanInfo above
-					maxLoanAmt: 11000,
-					loanTerm36: true,
-					loanTerm60: true,
-					'0': null,
-					'1': {
-						apr36: 12.2,
-						apr60: 13.19
-					}
-				}
-			}
-		]
-	}
-];
+  // update with program names and corresponding loan URLs with market segment code from Master Loan Parameters
+  {
+    name: "Full-Stack Immersive In-Person",
+    url: "https://my.skills.fund/application?lenderCode=SKINVTI18",
+    loanInfo: {
+      // match loanInfo in first metro below
+      maxLoanAmt: 13000,
+      loanTerm36: true,
+      loanTerm60: true,
+      "Interest Only": {
+        k: 5,
+      },
+      "Immediate Repayment": {
+        apr36: 12.87,
+        apr60: 13.62,
+      },
+    },
+    defaultLoanType: "Immediate Repayment", // leave at 0 for interest-only, set to 1 for immediate repayment
+    showMetros: false, // true if there are multiple metros with different tuition amounts for the same program
+    showLoanTypes: false, // true if both IR and IO are available
+    loanTypes: ["Immediate Repayment"],
+    locations: ["Metro 1", "Metro 2", "Metro 3"],
+    metros: [
+      // list in same order as locations array above
+      {
+        location: "Metro 1",
+        loanInfo: {
+          // // match loanInfo to Program 1 above
+          maxLoanAmt: 13000,
+          loanTerm36: true,
+          loanTerm60: true,
+          "Interest Only": {
+            k: 5,
+          },
+          "Immediate Repayment": {
+            apr36: 12.87,
+            apr60: 13.62,
+          },
+        },
+      },
+    ],
+  },
+  {
+    name: "Full-Stack Immersive Online",
+    url: "https://my.skills.fund/application?lenderCode=SKINVTO18",
+    loanInfo: {
+      // match loanInfo in first metro below
+      maxLoanAmt: 11000,
+      loanTerm36: true,
+      loanTerm60: true,
+      "Interest Only": {
+        k: 5,
+      },
+      "Immediate Repayment": {
+        apr36: 12.87,
+        apr60: 13.62,
+      },
+    },
+    defaultLoanType: "Immediate Repayment", // leave at 0 for interest-only, set to 1 for immediate repayment
+    showMetros: false,
+    showLoanTypes: false,
+    loanTypes: ["Immediate Repayment"],
+    locations: ["Metro 1", "Metro 2"],
+    metros: [
+      {
+        location: "Metro 1",
+        loanInfo: {
+          // match loanInfo to Program 2 loanInfo above
+          maxLoanAmt: 11000,
+          loanTerm36: true,
+          loanTerm60: true,
+          "Interest Only": {
+            k: 5,
+          },
+          "Immediate Repayment": {
+            apr36: 12.87,
+            apr60: 13.62,
+          },
+        },
+      },
+    ],
+  },
+]
 
 // ***** BEGIN LOAN CALC TEXT INFO *****
 export const programMaxText =
-	'Choose the loan amount that works best for you. Borrow up to $13,000 for the Full-Stack In-Person program tuition or up to $11,000 for the Full-Stack Online program tuition.';
+  "Choose the loan amount that works best for you. Borrow up to $13,000 for the Full-Stack In-Person program tuition or up to $11,000 for the Full-Stack Online program tuition."
 
 export const paymentTable = {
-	headers: [ 'Program', 'Tuition', 'Cost of Living', 'Max Total' ],
-	data: [
-		{
-			name: 'PROGRAM 1',
-			tuition: '$13,495',
-			col: '$6,000',
-			max: '$19,495'
-		},
-		{
-			name: 'PROGRAM 2',
-			tuition: '$16,495',
-			col: '$6,000',
-			max: '$22,495'
-		},
-		{
-			name: 'PROGRAM 3',
-			tuition: '$13,495',
-			col: '--',
-			max: '$13,495'
-		}
-	],
-	show: false
-};
+  headers: ["Program", "Tuition", "Cost of Living", "Max Total"],
+  data: [
+    {
+      name: "PROGRAM 1",
+      tuition: "$13,495",
+      col: "$6,000",
+      max: "$19,495",
+    },
+    {
+      name: "PROGRAM 2",
+      tuition: "$16,495",
+      col: "$6,000",
+      max: "$22,495",
+    },
+    {
+      name: "PROGRAM 3",
+      tuition: "$13,495",
+      col: "--",
+      max: "$13,495",
+    },
+  ],
+  show: false,
+}
 
 // ***** END LOAN CALC TEXT INFO *****
 
